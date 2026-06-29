b127e4efee7a18b6220f0887876f964564d9f644
Lucky <lucky9verma@gmail.com>
2026-06-30 02:01:14 +0530

    Add initial project structure and Expense model implementation

---
+++ models/expense.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+class expense():
+    def __init__(self, amount, category, desc, date = None):
+        self.amount = amount
+        self.category = category
+        self.desc = desc
+
+        if(date == None):
+            self.date = datetime.now().strftime("%Y-%m-%d")
+        else:
+            self.date = date
+
+
+    def to_dict(self):
+        return{
+            "amount" : self.amount,
+            "category" : self.category,
+            "desc" : self.desc,
+            "date" : self.date
+        }           
+
+    def from_dict(cls, data):
+        """Create a Expense object from dictionary."""
+        return cls(
+            amount = data["amount"],
+            category = data["category"],
+            desc = data["desc"],
+            date = data["date"],
+        )
+    
+    def __str__(self):
+        return(
+            f"Date: {self.date}\n"
+            f"Category: {self.category}\n"
+            f"Description: {self.description}\n"
+            f"Amount: {self.amount:.2f}\n"
+        )
