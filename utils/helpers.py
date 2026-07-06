24e65e2f1c9988714c74e9ee1df119b57ae0916e
Lucky <lucky9verma@gmail.com>
2026-07-07 03:42:25 +0530

    Add helper functions for user input validation and menu display

---
+++ utils/helpers.py
@@ -0,0 +1,43 @@
+def display_menu():
+    print("""
+========== Expense Tracker ==========
+1. Add Expense
+2. View Expenses
+3. Delete Expense
+4. Search Expense
+5. Monthly Total
+6. Exit
+""")
+    
+def get_valid_amount():    
+    while True:
+        amount = input("Enter the amount : ")
+        try:
+            amount = float(amount)
+            if(amount > 0):
+                return amount
+        except ValueError:
+            print("Please Enter valid Amount : ")
+
+
+def get_valid_category():
+    categories = ("food", "shopping", "travel", "bills", "other")
+
+    while True:
+        category = input("Enter Category: ").strip().lower()
+        if category in categories:
+            return category
+        print("Please enter a valid category: food, shopping, travel, bills, other")
+    
+def get_valid_month(month):
+    if(month > 12 or month < 1):
+        return False
+    else:
+        return True
+
+def pause():
+    print("Press Enter to continue...")
+    user_input = input()
+    while(user_input==""):
+        return True
+    
